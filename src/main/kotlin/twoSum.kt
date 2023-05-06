3e1f6620efc7feb8f600af8dd77feae37300b5d9
ryangst <ryangst.hire@gmail.com>
2023-05-06 19:33:24 -0300

    solution: leetcode twoSum

---
+++ src/main/kotlin/twoSum.kt
@@ -0,0 +1,15 @@
+fun twoSum(nums: IntArray, target: Int): Array<Int> {
+    val map = HashMap<Int, Int>()
+
+    for ((index, value) in nums.withIndex()) {
+        val diff = target - value
+
+        if (map.containsKey(diff)) {
+            return arrayOf(map[diff]!!, index)
+        }
+
+        map[value] = index
+    }
+
+    return arrayOf()
+}
---
+++ src/test/kotlin/TwoSumKtTest.kt
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions.*
+
+class TwoSumKtTest {
+
+    @org.junit.jupiter.api.Test
+    fun twoSumTest() {
+
+        val result = twoSum(intArrayOf(2, 7, 11, 15), 9)
+        assertArrayEquals(arrayOf(0, 1), result)
+
+    }
+}
