f37ef88af93b4c50d23ce12eb8441cc6f9831688
ryangst <ryangst.hire@gmail.com>
2023-05-06 22:14:18 -0300

    solution: leetcode longestCommonPrefix

---
+++ src/main/kotlin/LongestCommonPrefix.kt
@@ -0,0 +1,13 @@
+fun longestCommonPrefix(strs: Array<String>): String {
+    if (strs.isEmpty()) return ""
+
+    var prefix = strs[0]
+
+    // start at 1 because 0 it's our prefix
+    for (i in 1 until strs.size) {
+        // remove one letter from prefix
+        while (strs[i].indexOf(prefix) != 0) prefix = prefix.substring(0, prefix.length - 1)
+        if (prefix.isEmpty()) return ""
+    }
+    return prefix
+}
---
+++ src/test/kotlin/LongestCommonPrefixKtTest.kt
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class LongestCommonPrefixKtTest {
+
+    @Test
+    fun longestCommonPrefixTest() {
+        val common = longestCommonPrefix(arrayOf("flower", "flow", "flight"))
+        assertEquals("fl", common)
+
+        val common2 = longestCommonPrefix(arrayOf("dog", "racecar", "car"))
+        assertEquals("", common2)
+
+        val common3 = longestCommonPrefix(arrayOf("a"))
+        assertEquals("a", common3)
+    }
+}
