a2248db44a8e0ea77f56aa8cad19aff4a237c8a6
ryangst <ryangst.hire@gmail.com>
2024-05-30 17:27:28 -0300

    testing again
    Signed-off-by: ryangst <ryangst.hire@gmail.com>

--- src/test/kotlin/leetcode/SingleNumKtTest.kt
+++ src/test/kotlin/leetcode/SingleNumKtTest.kt
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class SingleNumKtTest {
-
     @Test
     fun singleNumberTest() {
         val duplicate = singleNumber(intArrayOf(1, 2, 2))
